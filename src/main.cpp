eec22dfdbcbfdd58a8537bb932477d0be932cce1
Battino <battino46@gmail.com>
2024-07-03 13:06:32 +0200

    fix compiler

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,30 @@
 #include "main.h"
 #include "game.h"
 #include <iostream>
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
 using std::cout;
 using std::endl;
 
 int main(int argc, char **argv)
 {
   cout << "Hello, World!" << endl;
+  using namespace ftxui;
+
+  // Define the document
+  Element document =
+      hbox({
+          text("left") | border,
+          text("middle") | border | flex,
+          text("right") | border,
+      });
+
+  auto screen = Screen::Create(
+      Dimension::Full(),       // Width
+      Dimension::Fit(document) // Height
+  );
+  Render(screen, document);
+  screen.Print();
 
   Game game = Game();
   game.run();
